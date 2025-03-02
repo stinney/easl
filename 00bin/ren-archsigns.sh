f28600f1d0993961ad34cec6bf96071a6aa8955e
stinney <stinney@upenn.edu>
2025-03-02 11:36:22 -0500

    new set of OID-named files

--- 00bin/ren-archsigns.sh
+++ 00bin/ren-archsigns.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 rm -fr ghjpg ; mkdir ghjpg
-rocox -n -R"cp '../proto-cuneiform_signs/%6' ghjpg/%1.jpg" <oid-easl.tsv |
-    /bin/sh -s
-
+cat oid-easl.tsv | rocox -n -R'cp "../proto-cuneiform_signs/%6" ghjpg/%1.jpg' >cp.sh
